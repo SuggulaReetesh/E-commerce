bbcdb02abdbbde2a803b4be378b9fa50a235f655
reetesh suggula <reeteshsuggula@gmail.com>
2021-06-13 19:46:51 +0530

    Architecture of Project

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+namespace Core.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id {get;set;}
+        public string ProductName{get;set;}
+        
+    }
+}
